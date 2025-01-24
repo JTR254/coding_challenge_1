d9acf2606cdb7ce152ab7455f1caf1333c6300c8
JTR254 <jackrindge@gmail.com>
2025-01-24 10:12:33 -0500

    Task 1 - Employee Information

--- cc_1.js
+++ cc_1.js
@@ -1,9 +1,10 @@
 //Task 1
 
-let employeeName = "Michael"; //employee's name
+let employeeName = "Michael Wu"; //employee's name
 const employeeID = "1453"; //employee's ID number
 var isActive = true; //employee's current status
 console.log(typeof employeeName); 
 console.log(typeof employeeID); 
 console.log(typeof isActive); 
+
 
