5e79f731b5217b0ed4eb601c9eb5a6f47efe07cd
JTR254 <jackrindge@gmail.com>
2025-01-24 10:35:26 -0500

    Task 2 - Product Details; also editted Task 1

--- cc_1.js
+++ cc_1.js
@@ -1,10 +1,20 @@
 //Task 1
 
 let employeeName = "Michael Wu"; //employee's name
-const employeeID = "1453"; //employee's ID number
+const employeeID = 1453; //employee's ID number
 var isActive = true; //employee's current status
 console.log(typeof employeeName); 
 console.log(typeof employeeID); 
 console.log(typeof isActive); 
+
+//Task 2
+
+let productName = "Salt Shaker"; //name of the product
+const productPrice = 8.99; //price of the product
+var isAvaiable = true; //product availability status
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof isAvailable);
+
 
 
