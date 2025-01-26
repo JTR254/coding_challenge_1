ff73e649bca1d3d928d914a7264fdc91fd5a5118
JTR254 <jackrindge@gmail.com>
2025-01-26 15:04:47 -0500

    Task 3 - Financial Transactions

--- cc_1.js
+++ cc_1.js
@@ -10,11 +10,25 @@ console.log(typeof isActive);
 //Task 2
 
 let productName = "Salt Shaker"; //name of the product
-const productPrice = 8.99; //price of the product
-var isAvaiable = true; //product availability status
+const productPrice = 9.00; //price of the product
+var isAvailable = true; //product availability status
 console.log(typeof productName);
 console.log(typeof productPrice);
 console.log(typeof isAvailable);
 
+//Task 3
+
+let accountBalance = 2500;
+console.log ("Beginning Account Balance", accountBalance);
+accountBalance -= 500; // decreases account balance by 500
+console.log ("Ending Account Balance", accountBalance);
+
+let discountRate = .15; // 15% discount
+let discountAmount = productPrice * discountRate; // 9.00 x .15 = 1.35
+let finalValue = productPrice - discountAmount; // 9.00 - 1.35
+
+console.log ("Discount Rate", discountRate);
+console.log ("Discount Amount", discountAmount);
+console.log ("Final Price After Discount", finalValue);
 
 
